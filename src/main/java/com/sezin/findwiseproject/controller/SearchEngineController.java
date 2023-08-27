22ea0500f44ed85464934e3ce4ddc0338d4027a8
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2023-08-28 00:50:07 +0200

    Take query from user with scanner

--- src/main/java/com/sezin/findwiseproject/controller/SearchEngineController.java
+++ src/main/java/com/sezin/findwiseproject/controller/SearchEngineController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Scanner;
 
 @Controller
 public class SearchEngineController {
@@ -40,11 +41,33 @@ public class SearchEngineController {
         //Creates a documentList and then creates inverted index from this documentList
         Set<Document> documentList = createDocuments();
         searchEngineService.createInvertedIndex(documentList);
-        List<IndexEntry> foundDocuments = searchEngineService.search("fox");
 
-        //Creates a result list that holds document names sorted by their tfidf scores
-        List<String> resultList = searchEngineService.createResultList(foundDocuments);
+        List<String> resultList = takeTermFromUser();
+        return resultList;
+    }
 
+    private List<String> takeTermFromUser() {
+
+        List<String> resultList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print("Enter a search query (or 'q' to exit): ");
+            String query = scanner.nextLine();
+            if (query.equalsIgnoreCase("q")) {
+                System.out.println("SearchEngine ended!.");
+                scanner.close();
+                break;
+            }
+            List<IndexEntry> foundDocuments = searchEngineService.search(query);
+
+            //Creates a result list that holds document names sorted by their tfidf scores
+            resultList = searchEngineService.createResultList(foundDocuments);
+            if(resultList.isEmpty()) {
+                System.out.println("No results found.");
+            }else{
+                System.out.println(resultList.toString());
+            }
+        }
         return resultList;
     }
 

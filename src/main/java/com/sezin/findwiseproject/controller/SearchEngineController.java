e1e939b11ff979cffbc93a86c40ff6e9d57b8b94
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2023-08-28 17:14:51 +0200

    Reading filepath of documents and contents are added as a comment

--- src/main/java/com/sezin/findwiseproject/controller/SearchEngineController.java
+++ src/main/java/com/sezin/findwiseproject/controller/SearchEngineController.java
@@ -5,6 +5,7 @@ import com.sezin.findwiseproject.model.Document;
 import com.sezin.findwiseproject.service.SearchEngineService;
 import org.springframework.stereotype.Controller;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -21,17 +22,51 @@ public class SearchEngineController {
     }
 
     public Set<Document> createDocuments(){
-        String documents1 = "the brown fox jumped over the brown dog";
-        String documents2 = "the lazy brown dog sat in the corner";
-        String documents3 = "the red fox bit the lazy dog";
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a filepath:");
+        String filePath = scanner.nextLine();
+
+        //Creates a documentList set with documents that are given by user
+        Set<Document> documentList = new HashSet<>();
+        try {
+            File[] allfiles = new File(filePath).listFiles();
+            BufferedReader in = null;
+            for (File f : allfiles) {
+                if (f.getName().endsWith(".txt")) {
+                    in = new BufferedReader(new FileReader(f));
+                    StringBuilder sb = new StringBuilder();
+                    String s = null;
+                    while ((s = in.readLine()) != null) {
+                        sb.append(s);
+                    }
+                    documentList.add(searchEngineService.indexDocument(f.getName(), String.valueOf(sb)));
+                }
+                else {
+                    continue;
+                }
+            }
+        }catch(FileNotFoundException exception) {
+            System.out.println("File not found "+exception);
+        }
+        catch(NullPointerException exception) {
+            System.out.println("Directory name is empty "+exception);
+        }
+        catch(IOException exception) {
+            System.out.println("IOException "+exception);
+        }*/
+
+        String document1 = "the brown fox jumped over the brown dog";
+        String document2 = "the lazy brown dog sat in the corner";
+        String document3 = "the red fox bit the lazy dog";
 
         //Creates a documentList set with documents that are given by user
         Set<Document> documentList = new HashSet<>();
 
         //Calls indexDocument method in order to split and parse documents into a wordList list and stores this list into a document objects
-        documentList.add(searchEngineService.indexDocument("documents1", documents1));
-        documentList.add(searchEngineService.indexDocument("documents2", documents2));
-        documentList.add(searchEngineService.indexDocument("documents3", documents3));
+        documentList.add(searchEngineService.indexDocument("document 1", document1));
+        documentList.add(searchEngineService.indexDocument("document 2", document2));
+        documentList.add(searchEngineService.indexDocument("document 3", document3));
 
         return documentList;
 
@@ -70,5 +105,4 @@ public class SearchEngineController {
         }
         return resultList;
     }
-
 }
